679446c0aee143c54a471286793e0da441b6b0a9
FIRST_NAME LAST_NAME <jarrettflowers@hotmail.com>
2024-01-23 20:39:16 -0500

    Initial commit

---
+++ src/gui/Main.java
@@ -0,0 +1,19 @@
+package gui;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                MainWindow main = new MainWindow();
+                main.show();
+            }
+        });
+    }
+
+
+}

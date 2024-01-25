c89c81c88e8ba34634a87642f8b007b85ff416a0
_little_Engine <37882464+JarrettSF@users.noreply.github.com>
2024-01-24 20:56:49 -0500

    Update MainWindow.java

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,11 +1,12 @@
 package gui;
-import javax.swing.JFrame;
-
+import javax.swing.*;
+import java.awt.*;
 
 public class MainWindow {
 
     private JFrame window;
 
+    //main window frame
     public MainWindow(){
 
     window = new JFrame();
@@ -14,6 +15,24 @@ public class MainWindow {
     window.setSize(800,500);
     window.setLocationRelativeTo(null);
 
+        /* main panel */
+    JPanel panel = new JPanel();
+
+        JTextField text = new JTextField("Username");
+        text.setPreferredSize(new Dimension(100,50));
+        panel.add(text);
+
+        JTextField text1 = new JTextField("Password");
+        text1.setPreferredSize(new Dimension(100,50));
+        panel.add(text1);
+
+        JButton button = new JButton("Submit");
+       // button.setLocation(null);
+        panel.add(button);
+
+
+
+        window.add(panel);
 
     }
 
